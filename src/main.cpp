daab33688e26fcba2a9c11d261e9102ec99cdbf6
Robson Costa <robson.costa.1980@gmail.com>
2017-12-03 09:34:03 -0200

    Created the main project structure.

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+
+#define LUMINOSITY_SENSOR   1
+
+void setup() {
+    // initialize LED digital pin as an output
+    pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+    // turn the LED on (HIGH is the voltage level)
+    digitalWrite(LED_BUILTIN, HIGH);
+
+    // wait for a second
+    delay(500);
+
+    // turn the LED off by making the voltage LOW
+    digitalWrite(LED_BUILTIN, LOW);
+
+    // wait for a second
+    delay(1000);
+}
