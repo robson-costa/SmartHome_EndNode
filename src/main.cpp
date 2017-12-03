6b1a5a5146e524b8c09868180facd23635fb9232
Robson Costa <robson.costa.1980@gmail.com>
2017-12-03 11:41:45 -0200

    Included LDR and DHT sensors.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
-//#include "DHT.h"
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
 
 // Main execution period (ms)
 #define PERIOD      1000
@@ -10,38 +12,47 @@
 #define SENSOR_DHT  1
 
 // LDR sensor configurations
-#ifdef SENSOR_LDR
+#if defined (SENSOR_LDR)
   #define SENSOR_LDR_PIN  A0
 #endif
 
 // PIR sensor configurations
-#ifdef SENSOR_PIR
+#if defined (SENSOR_PIR)
   #define SENSOR_PIR_PIN  7
 #endif
 
 // DHT sensor configurations
-#ifdef SENSOR_DHT
+#if defined (SENSOR_DHT)
   #define SENSOR_DHT_TYPE DHT11
   #define SENSOR_DHT_PIN  A1
 #endif
 
 // Functions declaration
-#ifdef SENSOR_LDR
-int getLDRValue(int samples, int interval);
+#if defined (SENSOR_LDR)
+  int getLDRValue(int samples, int interval);
+  float getTemperatureValue(int samples, int interval);
+  float getHumidityValue(int samples, int interval);
 #endif
+
+// Global variables
+DHT dht(SENSOR_DHT_PIN, SENSOR_DHT_TYPE);
 
 void setup() {
     // initialize LED digital pin as an output
     pinMode(LED_BUILTIN, OUTPUT);
 
-    #ifdef SENSOR_PIR
+    #if defined (SENSOR_PIR)
       // initialize PIR pin as an output
-      pinMode(SENSOR_LDR_PIN, OUTPUT);
+      pinMode(SENSOR_LDR_PIN, INPUT);
     #endif
 
-    #ifdef SENSOR_DHT
-
+    #if defined (SENSOR_DHT)
+      // initialize DHT sensor
+      dht.begin();
     #endif
+
+    // Configure serial OUTPUT
+    Serial.begin(115200);
 }
 
 void loop() {
@@ -52,24 +63,52 @@ void loop() {
       digitalWrite(LED_BUILTIN, LOW);
     }
 
-    #ifdef SENSOR_LDR
-      // Get the luminosity value
+    #if defined (SENSOR_LDR)
+      // Get the LDR (Luminosity) value
       int ldrValue = getLDRValue(10, 1);
     #endif
 
+    #if defined (SENSOR_PIR)
+      // Get the PIR (Presence) value
+      int pirValue = digitalRead(SENSOR_PIR_PIN);
+    #endif
+
+    #if defined (SENSOR_DHT)
+      // Get the DHT (Temperature) value
+      int tempValue = getTemperatureValue(10, 1);
+
+      // Get the DHT (Humidity) value
+      int humValue = getHumidityValue(10, 1);
+    #endif
+
     // Print the values on serial
-    Serial.print("#############################\r\n");
-    #ifdef SENSOR_PIR
-      Serial.print("Luminosity: ");
+    Serial.print("\r\n#############################");
+    #if defined (SENSOR_LDR)
+      Serial.print("\r\nLuminosity: ");
       Serial.print(ldrValue);
     #endif
-    Serial.print("\r\n#############################");
+    #if defined (SENSOR_PIR)
+      Serial.print("\r\nPresence: ");
+      if (pirValue == LOW) {
+        Serial.print("STOPPED!!!");
+      } else {
+        Serial.print("MOVIMENT!!!");
+      }
+    #endif
+    #if defined (SENSOR_DHT)
+      Serial.print("\r\nTemperature: ");
+      Serial.print(tempValue);
+      Serial.print(" oC\r\nHumidity: ");
+      Serial.print(humValue);
+      Serial.print("%");
+    #endif
+    Serial.print("\r\n#############################\r\n");
 
     // Wait the Main Execution Period
     delay(PERIOD);
 }
 
-#ifdef SENSOR_PIR
+#if defined (SENSOR_LDR)
 /* Get the LDR ldrValue
  * params: int samples - number of samples
  *         int interval - interval between samples
@@ -79,6 +118,36 @@ int getLDRValue(int samples, int interval) {
   unsigned int value = 0;
   for (int i = 0; i < samples; i++) {
     value += analogRead(SENSOR_LDR_PIN);
+    delay(interval);
+  }
+  return value/samples;
+}
+#endif
+
+#if defined (SENSOR_DHT)
+/* Get the DHT (Temperature) value
+ * params: int samples - number of samples
+ *         int interval - interval between samples
+ * return: float media between temperature values sampled
+ */
+float getTemperatureValue(int samples, int interval) {
+  float value = 0;
+  for (int i = 0; i < samples; i++) {
+    value += dht.readTemperature();
+    delay(interval);
+  }
+  return value/samples;
+}
+
+/* Get the DHT (Humidity) value
+ * params: int samples - number of samples
+ *         int interval - interval between samples
+ * return: float media between humidity values sampled
+ */
+float getHumidityValue(int samples, int interval) {
+  float value = 0;
+  for (int i = 0; i < samples; i++) {
+    value += dht.readHumidity();
     delay(interval);
   }
   return value/samples;
